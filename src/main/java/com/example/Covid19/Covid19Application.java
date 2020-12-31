53eb1f486782f0461d01f60a827e239970a42c88
Souvik dey <38080237+svkdey@users.noreply.github.com>
2020-12-31 11:19:15 +0530

    removing batch config

--- src/main/java/com/example/Covid19/Covid19Application.java
+++ src/main/java/com/example/Covid19/Covid19Application.java
@@ -16,7 +16,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableBatchProcessing
+//@EnableBatchProcessing
 @EnableScheduling
 @EnableSwagger2
 @EnableCaching
